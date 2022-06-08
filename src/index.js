01c79e9d844f6b404732844a623a7686f5e80072
Soboleva Natalia <nsoboleva1979@rambler.ru>
2022-06-08 20:21:47 +0300

    Change files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 import './js/script';
 import './css/style.css';
-import './pic/goblin.png';
--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,8 @@
+import image from '../pic/goblin.png';
+
 const cont = document.querySelector('.container');
 
-export function getElement() {
-  const el = document.createElement('div');
-  el.classList.add('item');
-  return el;
-}
-
-for (let i = 0; i < 16; i += 1) {
-  const item = getElement();
-  cont.append(item);
-}
-
-const items = document.querySelectorAll('.item');
-export const itemsArray = Array.from(items);
-
+// функция возвращающая случайное число в заданном диапазоне
 export function getRandomInt(min, max) {
   const mini = Math.ceil(min);
   const maxi = Math.floor(max);
@@ -21,13 +10,28 @@ export function getRandomInt(min, max) {
   // Максимум не включается, минимум включается
 }
 
+// функция создающая определенный HTML-элемент с заданным классом
+export function getElement(tag, clas) {
+  const el = document.createElement(tag);
+  el.classList.add(clas);
+  return el;
+}
+
+for (let i = 0; i < 16; i += 1) {
+  const item = getElement('div', 'item');
+  cont.append(item);
+}
+
+const items = document.querySelectorAll('.item');
+export const itemsArray = Array.from(items);
+
+// перемещение элемента по DOM-дереву в пределах одной коллекции
 setInterval(() => {
-  const imeg = document.createElement('img');
-  imeg.src = '../pic/goblin.png';
-  imeg.classList.add('image');
+  const imeg = getElement('img', 'image');
+  imeg.src = image;
   const index = getRandomInt(0, 16);
   itemsArray[index].appendChild(imeg);
   setTimeout(() => {
     imeg.remove();
-  }, 2000);
-}, 3000);
+  }, 1000);
+}, 2000);
